794cb02ab18f25bf0208a9f5d72eb20f400afb8d
shivanivinyakya <shivanivinyakya1996@gmail.com>
2026-06-15 04:11:53 -0400

    5th commit 2nd part

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,11 +1,20 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+//const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
 
 
-const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
-document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+const breakfastMenu = [
+    'Pancakes - $12.00',
+    'Eggs Benedict - $22.99',
+    'Oatmeal - $21.99',
+    'Frittata - $15.00'
+];
 
+const breakfastMenuItemsHTML = breakfastMenu
+    .map((item, index) => `<p>Item ${index + 1}: ${item}</p>`)
+    .join('');
+
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
 
 let mainCourseItem = '';
 mainCourseMenu.forEach((item, index) => {
